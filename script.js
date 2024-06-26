a58d2d3bacebea2e3da665c7025a7830cf21a453
Chrisgj22 <121605828+Chrisgj22@users.noreply.github.com>
2024-06-26 23:33:24 +1000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+let hamburger = document.getElementById("toggleMenu")
+let mobileMenu = document.getElementById("menuToggle")
+let closeMenu = document.getElementById("closeMenu")
+
+
+hamburger.addEventListener("click",function() {
+    mobileMenu.style.display = "flex"
+    })
+
+
+closeMenu.addEventListener("click",function() {
+    mobileMenu.style.display = "none"
+    })
