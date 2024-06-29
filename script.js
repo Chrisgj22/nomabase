31180faddcbf75687b8de58af4e8038730b04659
Chrisgj22 <121605828+Chrisgj22@users.noreply.github.com>
2024-06-29 17:59:21 +1000

    1.1.1

--- script.js
+++ script.js
@@ -15,15 +15,3 @@ closeMenu.addEventListener("click",function() {
 
 
 
-  
-   const scriptURL = 'https://script.google.com/macros/s/AKfycby_vAtWABVl7fP_kviUC5513AA8asbWNlVm9KxTgymB03Snw75LJFr3SR15vk7h6ulr/exec'
-   
-   const form = document.forms['subscriber']
-   
-   form.addEventListener('submit', e => {
-     e.preventDefault()
-     fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-     .then(response => alert("Thank you! your form is submitted successfully." ))
-     .then(() => { window.location.reload(); })
-     .catch(error => console.error('Error!', error.message))
-   })
