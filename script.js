c0d798318de661c0529cb85f82ac4af252bd1dd0
Chrisgj22 <121605828+Chrisgj22@users.noreply.github.com>
2024-06-30 16:36:10 +1000

    1.0

--- script.js
+++ script.js
@@ -12,26 +12,17 @@ closeMenu.addEventListener("click",function() {
     mobileMenu.style.display = "none"
     })
 
-'https://script.google.com/macros/s/AKfycbzfb3gM8rmc3usKQwUiCJqxEd7Yiou71SZvNb0U0vdIWIPrCjV7GTxYIF_OULnq072S6Q/exec'
+//'https://script.google.com/macros/s/AKfycbzfb3gM8rmc3usKQwUiCJqxEd7Yiou71SZvNb0U0vdIWIPrCjV7GTxYIF_OULnq072S6Q/exec'//
+
 
 function submitForm() {
-    const url = 'https://script.google.com/macros/s/AKfycbzfb3gM8rmc3usKQwUiCJqxEd7Yiou71SZvNb0U0vdIWIPrCjV7GTxYIF_OULnq072S6Q/exec'; // Replace with your script URL
+    const url = 'https://script.google.com/macros/s/AKfycbzfb3gM8rmc3usKQwUiCJqxEd7Yiou71SZvNb0U0vdIWIPrCjV7GTxYIF_OULnq072S6Q/exec'; // Replace with your form submission endpoint
 
-    $.ajax({
-      url: url,
-      method: 'POST',
-      data: $('#myForm').serialize(),
-      dataType: 'json',
-      success: function(response) {
-        if (response.result === 'success') {
-          // Redirect to thank-you page on successful response
-          window.location.href = "https://nomabase.com/thank-you.html";
-        } else {
-          console.error('Error:', response.error);
-        }
-      },
-      error: function(err) {
-        console.error('AJAX error:', err);
-      }
-    });
-  }
+    fetch(url, {
+        method: 'POST',
+        body: new FormData(document.getElementById('myForm'))
+    })
+    .then (
+        window.location.href = 'https://nomabase.com/thank-you'
+        )}
+       
