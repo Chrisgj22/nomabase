a5d6260ec0859107cae3fe666ad220c5b06e79c7
Chrisgj22 <121605828+Chrisgj22@users.noreply.github.com>
2024-06-30 15:57:13 +1000

    0.1

--- script.js
+++ script.js
@@ -14,4 +14,22 @@ closeMenu.addEventListener("click",function() {
 
 
 
+function submitForm() {
+    const url = 'https://script.google.com/macros/s/AKfycbzfb3gM8rmc3usKQwUiCJqxEd7Yiou71SZvNb0U0vdIWIPrCjV7GTxYIF_OULnq072S6Q/exec'; // Replace with your script URL
 
+    $.ajax({
+        url: url,
+        method: 'POST',
+        data: $('#myForm').serialize(),
+        success: function(response) {
+            // Handle success response
+            console.log(response);
+            // Redirect on success
+            window.location = "https://nomabase.com/thank-you.html";
+        },
+        error: function(err) {
+            // Handle error response
+            console.log(err);
+        }
+    });
+}
